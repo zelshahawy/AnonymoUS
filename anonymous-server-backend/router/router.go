94d1933ffd79047e70d6ae3e9337c716e53eb2b1
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-06-02 21:27:20 -0500

    removed origin cors

--- anonymous-server-backend/router/router.go
+++ anonymous-server-backend/router/router.go
@@ -19,7 +19,6 @@ func StartServer() {
 
 	// Set up CORS middleware
 	corsMiddleware := handlers.CORS(
-		handlers.AllowedOrigins([]string{"https://anonymous-sigma-three.vercel.app/login"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 		handlers.AllowCredentials(),
